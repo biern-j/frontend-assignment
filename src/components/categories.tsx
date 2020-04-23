e7145dfcc467c5c8cfca088410144d1213e4b378
Jola Wroniecka <wronieckajolanta@gmail.com>
2020-04-23 14:27:02 +0200

    Handle only one category to chose

--- src/components/categories.tsx
+++ src/components/categories.tsx
@@ -5,23 +5,26 @@ type CategoriesProps = {
   toggleCategory: (categoryName: string) => void;
 };
 export type Category = { category: string; checked: boolean };
-export const CategoriesChecklist = (props: CategoriesProps) =>
-  props.categories.length === 1 ? (
-    <div>"No category to choose"</div>
-  ) : (
-    <form>
-      <p>Data Soource</p>
-      {props.categories.map(category => (
-        <label key={category.category}>
-          {category.category}
-          <input
-            name={category.category}
-            type="checkbox"
-            checked={category.checked}
-            value={category.category}
-            onChange={e => props.toggleCategory(e.target.name)}
-          />
-        </label>
-      ))}
-    </form>
-  );
+export const CategoriesChecklist = (props: CategoriesProps) => (
+  <div>
+    <p>Data Source: </p>
+    {props.categories.length === 1 ? (
+      <div>{props.categories[0].category}</div>
+    ) : (
+      <form>
+        {props.categories.map(category => (
+          <label key={category.category}>
+            {category.category}
+            <input
+              name={category.category}
+              type="checkbox"
+              checked={category.checked}
+              value={category.category}
+              onChange={e => props.toggleCategory(e.target.name)}
+            />
+          </label>
+        ))}
+      </form>
+    )}
+  </div>
+);
